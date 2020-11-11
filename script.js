4332a44f37954511f1dd128404acd3ed7f918ba2
Douglas alves Baptista <douglasalvesbap@gmail.com>
2020-11-11 19:10:10 -0300

    aplication external folder

---
+++ script.js
@@ -0,0 +1,8 @@
+function clicar(){
+    var a = window.document.getElementById('agendamento')
+    a.style.background = 'blue'
+}
+function sair(){
+    var a = window.document.getElementById('agendamento')
+    a.style.background = 'rgba(0, 255, 0, 0.904)'
+}
